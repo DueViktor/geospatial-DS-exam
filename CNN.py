64c3bdeaf9187cd3d36e366993d98c90edeb6e50
DueViktor <viktorduep@gmail.com>
2023-05-11 12:38:16 +0200

    added maxpooling and upsampling

--- CNN.py
+++ CNN.py
@@ -13,35 +13,47 @@ class CNN(nn.Module):
 
         self.conv1 = nn.Conv2d(
             in_channels=num_input_channels,
-            out_channels=4,
+            out_channels=conv_filters1,
             kernel_size=3,
             padding=1,
         )
         self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.conv2 = nn.Conv2d(
-            in_channels=4,
-            out_channels=8,
+            in_channels=conv_filters1,
+            out_channels=conv_filters2,
             kernel_size=3,
             padding=1,
         )
         self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.conv3 = nn.Conv2d(
-            in_channels=8,
-            out_channels=4,
+            in_channels=conv_filters2,
+            out_channels=conv_filters3,
             kernel_size=3,
             padding=1,
         )
         self.relu3 = nn.ReLU()
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.conv4 = nn.Conv2d(in_channels=4, out_channels=1, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(
+            in_channels=conv_filters3, out_channels=1, kernel_size=3, padding=1
+        )
+        self.upsample = nn.Upsample(
+            size=(256, 256), mode="bilinear", align_corners=False
+        )
 
     def forward(self, x):
         c = self.conv1(x)
         x = self.relu1(c)
+        x = self.pool1(x)
         x = self.relu2(self.conv2(x))
+        x = self.pool2(x)
         x = self.relu3(self.conv3(x))
+        x = self.pool3(x)
         x = self.conv4(x)
+        x = self.upsample(x)
 
         return x
