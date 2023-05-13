7638848fe38a9ce275fbe24d3415c24a734962c4
DueViktor <viktorduep@gmail.com>
2023-05-13 10:58:36 +0200

    improved readability

--- CNN.py
+++ CNN.py
@@ -49,11 +49,12 @@ class CNN(nn.Module):
         c = self.conv1(x)
         x = self.relu1(c)
         x = self.pool1(x)
-        x = self.relu2(self.conv2(x))
+        x = self.conv2(x)
+        x = self.relu2(x)
         x = self.pool2(x)
-        x = self.relu3(self.conv3(x))
+        x = self.conv3(x)
+        x = self.relu3(x)
         x = self.pool3(x)
         x = self.conv4(x)
         x = self.upsample(x)
-
         return x
